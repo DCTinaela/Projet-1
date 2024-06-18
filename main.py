58470cb79c252fecf8d6dcb731e85fc5cdd14651
DCTinaela <ustinaela@gmail.com>
2024-06-18 17:00:48 +0300

    nice

---
+++ main.py
@@ -0,0 +1,14 @@
+#Wsh mon pote ça va ?
+if 5 > 2:
+    print("Five is greater than two!") 
+if 5 > 2:
+    print("Five is greater than two!") 
+x, y, z = "redmi", "samsung", "apple"
+print (x)
+print (y)
+print (z)
+marque = ["redmi", "samsung", "apple"]
+x, y, z = marque
+print (x)
+print (y)
+print (z)
